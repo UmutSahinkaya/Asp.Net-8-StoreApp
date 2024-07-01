645962d2a51bcdca9a1e2161267fd830bc9adc9b
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-07-01 12:03:56 +0300

    Cart ve CartItem nesneleri oluşturuldu.

---
+++ StoreApp.Web/Pages/Cart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StoreApp.Web.Pages
+{
+    public class CartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
