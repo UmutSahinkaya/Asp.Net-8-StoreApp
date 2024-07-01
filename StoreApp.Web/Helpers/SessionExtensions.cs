d4cff1cee033d8fe62b8cc1192a6459f776bfaa0
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-07-01 13:52:17 +0300

    Cart Table

--- StoreApp.Web/Helpers/SessionExtensions.cs
+++ StoreApp.Web/Helpers/SessionExtensions.cs
@@ -1,18 +1,17 @@
 ﻿using System.Text.Json;
 
-namespace StoreApp.Web.Helpers
-{
-    public static class SessionExtensions
-    {
-        public static void SetJson(this ISession session,string key,object value)
-        {
-            session.SetJson(key, JsonSerializer.Serialize(value));
-        }
+namespace StoreApp.Web.TagHelpers;
 
-        public static T? GetJson<T>(this ISession session,string key)
-        {
-            var sessionData=session.GetString(key);
-            return sessionData== null ? default(T) : JsonSerializer.Deserialize<T>(sessionData);
-        }
+public static class SessionExtensions
+{
+    public static void SetJson(this ISession session, string key, object value)
+    {
+        session.SetString(key, JsonSerializer.Serialize(value));
+    }
+
+    public static T? GetJson<T>(this ISession session, string key)
+    {
+        var sessionData = session.GetString(key);
+        return sessionData == null ? default(T) : JsonSerializer.Deserialize<T>(sessionData);
     }
 }
